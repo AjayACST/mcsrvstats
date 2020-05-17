ff28a3eaaceb22abd6619825fdaa257362920268
AjayACST <35202521+AjayACST@users.noreply.github.com>
2020-05-17 15:00:30 +1200

    Added Hypixel Game Stats
    Added Hypixel Game Stats

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import aiohttp
 import asyncio
+import requests
+import json
 from bs4 import BeautifulSoup
 
 async def get_html(url, session):
@@ -10,13 +12,51 @@ async def get_html(url, session):
         else:
             return False
 
-async def get_json(url, session):
-    async with session.get(url) as resp:
-        if resp.status == 200:
-            html = await resp.json()
-            return html
-        else:
-            return False
+async def get_json(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        json_src = r.json()
+        return json_src
+    else:
+        return False
+
+async def hypixelHungerGames(username,key):
+    url = f"https://api.hypixel.net/player?key={key}&name={username}"
+    json_data = await get_json(url)
+    str_json = json.dumps(json_data)
+    json_new = json.loads(str_json)
+    if json_new['player'] is None:
+        return False
+    else:
+        json_hg = json_new['player']['stats']['HungerGames'] if 'HungerGames' in json_new['player']['stats'] else 0
+        json_mcgo = json_new['player']['stats']['MCGO'] if 'MCGO' in json_new['player']['stats'] else 0
+        json_arcade = json_new['player']['stats']['Arcade'] if 'Arcade' in json_new['player']['stats'] else 0
+        json_gb = json_new['player']['stats']['GingerBread'] if 'GingerBread' in json_new['player']['stats'] else 0
+        json_pb = json_new['player']['stats']['Paintball'] if 'Paintball' in json_new['player']['stats'] else 0
+        json_quake = json_new['player']['stats']['Quake'] if 'Quake' in json_new['player']['stats'] else 0
+        json_vampirez = json_new['player']['stats']['VampireZ'] if 'VampireZ' in json_new['player']['stats'] else 0
+        json_legacy = json_new['player']['stats']['Legacy'] if 'Legacy' in json_new['player']['stats'] else 0
+        json_walls = json_new['player']['stats']['Walls'] if 'Walls' in json_new['player']['stats'] else 0
+        json_bedwars = json_new['player']['stats']['Bedwars'] if 'Bedwars' in json_new['player']['stats'] else 0
+        json_walls3 = json_new['player']['stats']['Walls3'] if 'Walls3' in json_new['player']['stats'] else 0
+        json_tnt = json_new['player']['stats']['TNTGames'] if 'TNTGames' in json_new['player']['stats'] else 0
+        json_arena = json_new['player']['stats']['Arena'] if 'Arena' in json_new['player']['stats'] else 0
+        json_uhc = json_new['player']['stats']['UHC'] if 'UHC' in json_new['player']['stats'] else 0
+        json_skywars = json_new['player']['stats']['SkyWars'] if 'SkyWars' in json_new['player']['stats'] else 0
+        json_tc = json_new['player']['stats']['TrueCombat'] if 'TrueCombat' in json_new['player']['stats'] else 0
+        json_mm = json_new['player']['stats']['MurderMystery'] if 'MurderMyster' in json_new['player']['stats'] else 0
+        json_sc = json_new['player']['stats']['SkyClash'] if 'SkyClash' in json_new['player']['stats'] else 0
+        json_bg = json_new['player']['stats']['Battleground'] if 'Battleground' in json_new['player']['stats'] else 0
+        json_duels = json_new['player']['stats']['Duels'] if 'Duels' in json_new['player']['stats'] else 0
+        json_bb = json_new['player']['stats']['BuildBattle'] if 'BuildBattle' in json_new['player']['stats'] else 0
+        json_pit = json_new['player']['stats']['Pit'] if 'Pit' in json_new['player']['stats'] else 0
+        json_sb = json_new['player']['stats']['SkyBlock'] if 'SkyBlock' in json_new['player']['stats'] else 0
+        data = {'HungerGames': [json_hg], 'MCGO': [json_mcgo], 'Arcade': [json_arcade], 'GingerBread': [json_gb], 'Paintball': [json_pb], 'Quake': [json_quake], 
+        'VampireZ': [json_vampirez], 'Legacy': [json_legacy], 'Walls': [json_walls], 'Bedwars': [json_bedwars], 'Walls3': [json_walls3], 'TNTGames': [json_tnt], 
+        'Arena': [json_arena], 'UHC': [json_uhc], 'SkyWars': [json_skywars], 'TrueCombat': [json_tc], 'MuderMystery': [json_mm], 'Battleground': [json_bg], 'Duels': [json_duels],
+        'BuildBattle': [json_bb], 'Pit': [json_pit]}
+        return data
+    
 
 async def blocksmc(username, session):
     url = f"https://blocksmc.com/player/{username}"
@@ -109,11 +149,15 @@ async def veltpvp(username, session):
         data["game_stats"].append({game_name: stats})
     return data
 
-async def run_def(username):
+"""async def run_def(username):
     async with aiohttp.ClientSession() as session:
-        print(await veltpvp(username, session))
+        print(await veltpvp(username, session))"""
+
+async def run_def(username):
+    await hypixelHungerGames(username, "218c4847-450e-4218-aa99-bcc08c7a6595")
+
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_def("DISSTRACKS"))
+    loop.run_until_complete(run_def(""))
     loop.close()
