45544e7086fa2882961f662656139ded2dc95d15
Darkflame72 <leon@neptune.localdomain>
2020-07-09 16:46:20 +1200

    fixed minesage as should have been list not dict

--- main.py
+++ main.py
@@ -20,17 +20,14 @@ async def get_json(url, session):
         return False
 
 
-
 async def hiveMCAchievements(username, session):
     url = f"http://api.hivemc.com/v1/player/{username}"
     json_data = await get_json(url, session)
-    str_json = json.dumps(json_data)
-    json_new = json.loads(str_json)
     data = {"all_achievements": []}
-    if json_new == False:
+    if json_data == False:
         return False
-    for ach in json_new['achievements']:
-        data['all_achievements'].append(ach)
+    for ach in json_data["achievements"]:
+        data["all_achievements"].append(ach)
     return data
 
 
@@ -42,9 +39,9 @@ async def hiveMCStatus(username, session):
     if json_new == False:
         return False
     data = {"status": []}
-    for _ in json_new['status']:
-        thing = json_new['status']
-        data['status'].append(thing)
+    for _ in json_new["status"]:
+        thing = json_new["status"]
+        data["status"].append(thing)
     return data
 
 
@@ -66,7 +63,7 @@ async def hiveMCRank(username, session):
     json_new = json.loads(str_json)
     if json_new == False:
         return False
-    rank = json_new['rankName']
+    rank = json_new["rankName"]
     data = {"rank": [rank]}
     return data
 
@@ -81,21 +78,26 @@ async def manacube(username, session, game):
     return data
 
 
-
 async def wyncraftClasses(username, session):
     url = f"https://api.wynncraft.com/v2/player/{username}/stats"
     json_data = await get_json(url, session)
     str_json = json.dumps(json_data)
     json_new = json.loads(str_json)
     data = {"classes": []}
-    if json_new['code'] == 400:
+    if json_new["code"] == 400:
         return False
-    json_len = len(json_new['data'][0]['classes'])
+    json_len = len(json_new["data"][0]["classes"])
     for i in range(json_len):
-        class_name = json_new['data'][0]['classes'][i]['name']
-        class_level = json_new['data'][0]['classes'][i]['level']
-        class_deaths = json_new['data'][0]['classes'][i]['deaths']
-        data['classes'].append({'class_name': class_name, 'class_level': class_level, 'class_deaths': class_deaths})
+        class_name = json_new["data"][0]["classes"][i]["name"]
+        class_level = json_new["data"][0]["classes"][i]["level"]
+        class_deaths = json_new["data"][0]["classes"][i]["deaths"]
+        data["classes"].append(
+            {
+                "class_name": class_name,
+                "class_level": class_level,
+                "class_deaths": class_deaths,
+            }
+        )
     """wynClasses = json_new['data'][0]['classes']
     data["classes"].append(wynClasses)"""
     return data
@@ -107,31 +109,31 @@ async def blocksmc(username, session):
     soup = BeautifulSoup(html, "lxml")
     if not soup.find("h1", {"class": "profile-header"}):
         return False
-    rank = soup.find("p", {"class": ["profile-rank"]}
-                     ).get_text().replace("\n", "").strip()
-    timeplayed = soup.find(
-        "h1", {"dir": ["ltr"]}).get_text().replace("\n", "").strip()
+    rank = (
+        soup.find("p", {"class": ["profile-rank"]}).get_text().replace("\n", "").strip()
+    )
+    timeplayed = soup.find("h1", {"dir": ["ltr"]}).get_text().replace("\n", "").strip()
     data = {"rank": rank, "timeplayed": timeplayed, "game_stats": []}
 
     for game in soup.find_all("div", {"class": "col-xl-4"}):
         stats = {}
-        game_name = game.find(
-            "div", {"class": "title"}).get_text().replace("\n", "").strip()
+        game_name = (
+            game.find("div", {"class": "title"}).get_text().replace("\n", "").strip()
+        )
         for stat in game.find_all("ul"):
-            stat_name = stat.find(
-                "div", {"class": "key"}).get_text().replace("\n", "").strip()
+            stat_name = (
+                stat.find("div", {"class": "key"}).get_text().replace("\n", "").strip()
+            )
             stat_val = int(stat.find("div", {"class": "val"}).get_text())
             stats[stat_name] = stat_val
         data["game_stats"].append({game_name: stats})
     return data
 
-# a bit of a time consumer will do later not finished yet
-
 
 async def universocraft(username, session):
     url = f"https://stats.universocraft.com/stats.php?player={username}"
     html = await get_html(url, session)
-    soup = BeautifulSoup(html, 'lxml')
+    soup = BeautifulSoup(html, "lxml")
     data = {"game_stats": []}
     for game in soup.find_all("div", {"class": "game"}):
         stats = {}
@@ -152,13 +154,21 @@ async def minesaga(username, session):
     joined = main_info.find("h4").get_text().strip()
     last_seen = main_info.findAll("span")[1].get_text().strip()
     play_time = main_info.findAll("span")[2].get_text().strip()
-    data = {"joined": joined, "last_seen": last_seen,
-            "play_time": play_time, "game_stats": {}}
+    data = {
+        "joined": joined,
+        "last_seen": last_seen,
+        "play_time": play_time,
+        "game_stats": [],
+    }
 
     for game in soup.find_all("div", {"class": "dd-section col-md-4"}):
         stats = {}
-        game_name = game.find(
-            "div", {"class": "dd-box-title"}).get_text().replace("\n", "").strip()
+        game_name = (
+            game.find("div", {"class": "dd-box-title"})
+            .get_text()
+            .replace("\n", "")
+            .strip()
+        )
         for stat in game.find_all("dl"):
             stat_name = stat.find("dt").get_text().replace("\n", "").strip()
             stat_val = stat.find("dd").get_text()
@@ -189,40 +199,54 @@ async def veltpvp(username, session):
     html = await get_html(url, session)
     soup = BeautifulSoup(html, "lxml")
     rank = soup.find("div", {"id": "profile"}).find("h2").get_text().strip()
-    last_seen = soup.find("div", {"class": "bottom"}).get_text().split("\n")[
-        2].replace("\xa0", " ").strip()
+    last_seen = (
+        soup.find("div", {"class": "bottom"})
+        .get_text()
+        .split("\n")[2]
+        .replace("\xa0", " ")
+        .strip()
+    )
     current_status = soup.find("div", {"class": "top"}).get_text().strip()
     info = soup.find_all("div", {"class": "element"})[1].get_text().split("\n")
     first_joined = info[3].strip()
     time_played = info[5].replace("\xa0", " ").strip()
     monthly_views = info[7].strip()
-    data = {"rank": rank, "last_seen": last_seen, "current_status": current_status,
-            "first_joined": first_joined, "time_played": time_played, "monthly_views": monthly_views, "game_stats": []}
+    data = {
+        "rank": rank,
+        "last_seen": last_seen,
+        "current_status": current_status,
+        "first_joined": first_joined,
+        "time_played": time_played,
+        "monthly_views": monthly_views,
+        "game_stats": [],
+    }
 
     # first stat is special
     first_game = soup.find("a", {"class": "server"})
-    game_name = first_game.find(
-        "div", {"class": "server-header"}).get_text().strip()
+    game_name = first_game.find("div", {"class": "server-header"}).get_text().strip()
     stats = {}
     for stat in first_game.find_all("div", {"class": "server-stat"}):
-        stat_name = stat.find(
-            "div", {"class": "server-stat-description"}).get_text().strip()
-        stat_val = stat.find(
-            "div", {"class": "server-stat-number"}).get_text().strip()
+        stat_name = (
+            stat.find("div", {"class": "server-stat-description"}).get_text().strip()
+        )
+        stat_val = stat.find("div", {"class": "server-stat-number"}).get_text().strip()
         stats[stat_name] = stat_val
     data["game_stats"].append({game_name: stats})
 
     for game in soup.find_all("div", {"class": "server"}):
         if not game.find("div", {"class": "server unknown"}):
             break
-        game_name = game.find(
-            "div", {"class": "server-header"}).get_text().strip()
+        game_name = game.find("div", {"class": "server-header"}).get_text().strip()
         stats = {}
         for stat in game.find_all("div", {"class": "server-stat"}):
-            stat_name = stat.find(
-                "div", {"class": "server-stat-description"}).get_text().strip()
-            stat_val = stat.find(
-                "div", {"class": "server-stat-number"}).get_text().strip()
+            stat_name = (
+                stat.find("div", {"class": "server-stat-description"})
+                .get_text()
+                .strip()
+            )
+            stat_val = (
+                stat.find("div", {"class": "server-stat-number"}).get_text().strip()
+            )
             stats[stat_name] = stat_val
         data["game_stats"].append({game_name: stats})
     return data
@@ -230,10 +254,10 @@ async def veltpvp(username, session):
 
 async def run_def(username):
     async with aiohttp.ClientSession() as session:
-        print(await wyncraftClasses(username, session))
+        print(await minesaga(username, session))
 
 
 if __name__ == "__main__":
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(run_def("_Tiger"))
+    loop.run_until_complete(run_def("Ambitions"))
     loop.close()
