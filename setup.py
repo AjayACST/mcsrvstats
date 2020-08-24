f47405be28d4e149cdf1586338d2249dd028b300
Darkflame72 <31436575+Darkflame72@users.noreply.github.com>
2020-08-25 11:48:33 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
+import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mcsrvstats",
+    version="0.1.0",
+    author="Leon Bowie",
+    author_email="leon@bowie-co.nz",
+    description="An async wrapper for popular minecraft servers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Obsidion-dev/mcsrvstats",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+    python_requires=">=3.6",
+    install_requires=["aiohttp",],
+)
