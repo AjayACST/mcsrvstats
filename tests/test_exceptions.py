b7938c613a53fcffd71463734e656521a6d08930
AjayACST <ajay@acst.co.nz>
2021-08-12 11:45:32 +1200

    Added new tests

---
+++ tests/test_exceptions.py
@@ -0,0 +1,20 @@
+import pytest
+from mcsrvstats import exceptions
+
+
+@pytest.mark.asyncio
+async def test_api_error() -> None:
+    """Check ApiError correctly gets raised."""
+
+    with pytest.raises(exceptions.exceptions.ApiError) as e:
+        raise exceptions.exceptions.ApiError("An error occurred.")
+    assert str(e.value) == "The unknown sourceAPI had An error occurred."
+
+
+@pytest.mark.asyncio
+async def test_player_not_found_error() -> None:
+    """Check PlayerNotFoundError correctly gets raised."""
+
+    with pytest.raises(exceptions.exceptions.PlayerNotFoundError) as e:
+        raise exceptions.exceptions.PlayerNotFoundError("Player not found.")
+    assert str(e.value) == "The player Player not found. could not be found."
--- tests/test_mcsrvstats.py
+++ tests/test_mcsrvstats.py
@@ -1,7 +1,46 @@
 """Main tests."""
+import pytest
+from aioresponses import aioresponses
 from mcsrvstats import __version__
+from mcsrvstats import Client
+from mcsrvstats import exceptions
 
 
 def test_version() -> None:
     """Mock version."""
     assert __version__ == "0.2.0"
+
+
+@pytest.mark.asyncio
+async def test_client_json(mcsrvstats_client: Client) -> None:
+    """Test JSON output from URL."""
+    with aioresponses() as m:
+        m.get(
+            "https://api.wynncraft.com/v2/player/IceWarox/stats",
+            status=200,
+            payload={
+                "kind": "wynncraft/player/IceWarox/stats",
+                "code": 200,
+                "timestamp": 1619819876728,
+                "version": "2.1.0",
+            },
+        )
+        client = mcsrvstats_client
+        data = await client._get_json(
+            "https://api.wynncraft.com/v2/player/IceWarox/stats"
+        )
+
+        assert data["kind"] == "wynncraft/player/IceWarox/stats"
+        assert data["code"] == 200
+        assert data["timestamp"] == 1619819876728
+        assert data["version"] == "2.1.0"
+
+
+@pytest.mark.asyncio
+async def test_client_json_error(mcsrvstats_client: Client) -> None:
+    """Test JSON output from URL when there is an error."""
+    with aioresponses() as m:
+        m.get("https://api.wynncraft.com/v2/player/IceWarox/stats", status=500)
+        client = mcsrvstats_client
+        with pytest.raises(exceptions.exceptions.ApiError):
+            await client._get_json("https://api.wynncraft.com/v2/player/IceWarox/stats")
