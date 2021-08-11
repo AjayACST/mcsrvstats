6b5b067c6ba98905e81cac56bafe17fcb57befba
AjayACST <ajay@acst.co.nz>
2021-08-12 11:49:17 +1200

    Fix flake errors

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,3 +1,4 @@
+"""Test for mcsrvstats exceptions."""
 import pytest
 from mcsrvstats import exceptions
 
@@ -5,7 +6,6 @@ from mcsrvstats import exceptions
 @pytest.mark.asyncio
 async def test_api_error() -> None:
     """Check ApiError correctly gets raised."""
-
     with pytest.raises(exceptions.exceptions.ApiError) as e:
         raise exceptions.exceptions.ApiError("An error occurred.")
     assert str(e.value) == "The unknown sourceAPI had An error occurred."
@@ -14,7 +14,6 @@ async def test_api_error() -> None:
 @pytest.mark.asyncio
 async def test_player_not_found_error() -> None:
     """Check PlayerNotFoundError correctly gets raised."""
-
     with pytest.raises(exceptions.exceptions.PlayerNotFoundError) as e:
         raise exceptions.exceptions.PlayerNotFoundError("Player not found.")
     assert str(e.value) == "The player Player not found. could not be found."
